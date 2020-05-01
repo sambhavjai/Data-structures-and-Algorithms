2f96b2e0580d52339015fe8fb41a22f41415b597
sambhavjai <38015768+sambhavjai@users.noreply.github.com>
2020-05-02 01:50:17 +0530

    math

---
+++ sqrt.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class sqrt{
+    // Find the floor of sqrt of the given number
+    // y = x^1/2;
+    // log10 y = 1/2 log10 x
+    // y=10^(1/2 log10 x)
+    public static int func(int x)
+    {
+        double z=Math.log10(x)/2.0;
+        return (int)(Math.pow(10,z));
+    }
+    public static void main(String args[])
+    {
+        int x=8;
+        System.out.println(func(x));
+    }
+}
